72f10e392790f6822cba7fc9420b18a41f32602c
Divyanshu Verma <divyanshu.verma1993@gmail.com>
2023-04-04 23:33:32 -0400

    fix imports with database

--- app/src/main/java/com/example/notesapp/AppDatabase.kt
+++ app/src/main/java/com/example/notesapp/AppDatabase.kt
@@ -1,9 +1,12 @@
 package com.example.notesapp
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(entities = [Note::class], version = 1)
-abstract class AppDatabase {
+abstract class AppDatabase: RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
     companion object {
